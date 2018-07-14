9a4e24e366a5f4a99dc9cccf490e92df1c3405dd
Olivier Mehani <shtrom@ssji.net>
2018-07-14 17:12:19 +1000

    Add bambam.activity
    Created with:
    
        curl -o http://dev.laptop.org/\~mcfletch/OLPCGames/OLPCGames-1.6.tar.gz
        tar xzvf OLPCGames-1.6.tar.gz
        cd OLPCGames-1.6/skeleton
        python2 buildskel.py -n bambam -t Bambam  -m bambam:main
        mv bambam.activity ../..
    
    as per instructions on the OLPC wiki [0].
    
    [0] http://wiki.laptop.org/go/Porting_pygame_games_to_the_XO
    
    Signed-off-by: Olivier Mehani <shtrom@ssji.net>

---
+++ bambam.activity/activity.py
@@ -0,0 +1,9 @@
+from olpcgames import activity
+from gettext import gettext as _
+
+class Activity(activity.PyGameActivity):
+    """Your Sugar activity"""
+    
+    game_name = 'bambam:main'
+    game_title = _('Bambam')
+    game_size = None
